96de66dc0a9420b14824ebbdf8ef867e0a9054ff
DonThuanUIT-24521736 <24521736@gm.uit.edu.vn>
2026-04-15 23:31:31 +0700

    feat(auth): config spring security, jwt util and custom user details

--- backend/src/main/java/com/foodorderingapp/backend/entity/User.java
+++ backend/src/main/java/com/foodorderingapp/backend/entity/User.java
@@ -8,7 +8,12 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -22,14 +27,14 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @EqualsAndHashCode.Include
     private UUID id;
 
-    @NotBlank(message = "Phone number cannot be left black")
+    @NotBlank(message = "Phone number cannot be left blank")
     @Column(nullable = false, unique = true)
     private String phone;
 
@@ -60,4 +65,36 @@ public class User extends BaseEntity {
 
     @Column(name = "bank_account_number")
     private String bankAccountNumber;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.phone;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.status == UserStatusEnum.ACTIVE;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.status == UserStatusEnum.ACTIVE;
+    }
 }
