55e02e6e8581d1a0b6324f040c53a5f249030d4d
NGUYENHUUTINH-uit <huutinhpc.2023@gmail.com>
2026-06-10 19:52:10 +0700

    feat: delete cartitem

--- backend/src/main/java/com/foodorderingapp/backend/service/CartService.java
+++ backend/src/main/java/com/foodorderingapp/backend/service/CartService.java
@@ -8,4 +8,5 @@ public interface CartService {
     void addToCart(UUID foodId, Integer quantity, String note, String phone);
     CartResponse getCart(String phone);
     void updateCartItemQuantity (UUID cartItemId, Integer quantity, String phone);
+    void deleteCartItem(UUID cartItemId, String phone);
 }
